f8d944a7c08f3955b84989f26acba593ee9220ce
EvgeniyHalimon <halimonevgeniy@gmail.com>
2022-02-08 18:55:46 +0200

    feat: added login and logout logic and cookies

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import express from 'express'
 import morgan from "morgan";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+dotenv.config()
 
 import authRoutes from './routes/auth'
 
@@ -12,12 +16,13 @@ const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(trim)
+app.use(cookieParser())
 
 app.get('/', (req, res) => res.send("Floppa send their regards"))
 app.use("/api/auth", authRoutes)
 
 
-app.listen(5000, async () => {
+app.listen(process.env.PORT, async () => {
     console.log("Server running on port 5000");
     try{    
         await createConnection()
