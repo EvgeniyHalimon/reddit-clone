d8f686a8e5a4be7e835c184ef130cf69d8ded830
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-04-05 12:19:35 +0300

    added new route, update server

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import authRoutes from './routes/auth'
 import postRoutes from './routes/posts'
 import subRoutes from './routes/subs'
 import miscRoutes from './routes/misc'
+import userRoutes from './routes/users'
 
 import trim from './middleware/trim'
 
@@ -22,11 +23,13 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use(trim)
 app.use(cookieParser())
-app.use(cors({
-  credentials: true,
-  origin: process.env.ORIGIN,
-  optionsSuccessStatus: 200
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+    optionsSuccessStatus: 200,
+  })
+)
 app.use(express.static('public'))
 
 app.get('/', (_, res) => res.send('Hello World'))
@@ -34,6 +37,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/posts', postRoutes)
 app.use('/api/subs', subRoutes)
 app.use('/api/misc', miscRoutes)
+app.use('/api/users', userRoutes)
 
 app.listen(PORT, async () => {
   console.log(`Server running at http://localhost:${PORT}`)
@@ -44,5 +48,4 @@ app.listen(PORT, async () => {
   } catch (err) {
     console.log(err)
   }
-  
 })
